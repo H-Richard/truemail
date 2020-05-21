1285d79645bcc3314c8ef1f9b8c9e9efa8df85f6
H-Richard <r.hong@pm.me>
2020-05-20 23:00:29 -0400

    Added Validate Function

--- truemail.go
+++ truemail.go
@@ -1,1 +1,17 @@
 package truemail
+
+import (
+	"errors"
+	"regexp"
+)
+
+var (
+	formatRegexp, _ = regexp.Compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$)")
+)
+
+func Validate(email string) error {
+	if !formatRegexp.MatchString(email) {
+		return errors.New("invalid format")
+	}
+	return nil
+}
