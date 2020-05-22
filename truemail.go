7a65a62cdc1107b46210351ba7108540206a1642
H-Richard <r.hong@pm.me>
2020-05-22 10:59:57 -0400

    Added domain validation

--- truemail.go
+++ truemail.go
@@ -2,16 +2,32 @@ package truemail
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
+	"strings"
 )
 
 var (
 	formatRegexp, _ = regexp.Compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$)")
 )
 
-func Validate(email string) error {
+//Validator struct
+type Validator struct {
+	Domains []string
+	Strict  bool
+}
+
+//Validate : validates emails
+func (v *Validator) Validate(email string) error {
 	if !formatRegexp.MatchString(email) {
 		return errors.New("invalid format")
+	}
+	if domains := v.Domains; len(domains) > 0 {
+		domainRegexp, _ := regexp.Compile(fmt.Sprintf(".*(%s)$", strings.Join(domains, "|")))
+		if !domainRegexp.MatchString(email) {
+			return errors.New("invalid domain")
+		}
+
 	}
 	return nil
 }
